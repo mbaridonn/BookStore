215ea5d25bfbc355eb75a7d789f89b2cb585f55b
Martiniano Baridon <mbaridonn@gmail.com>
2020-01-28 13:42:46 -0300

    Removed node_modules and dist from repository

--- server/dist/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = (app) => {
-    app.get("/asd", (req, res) => {
-        res.send("Asdasdsadsa");
-    });
-};
-//# sourceMappingURL=routes.js.map
