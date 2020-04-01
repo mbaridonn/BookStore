93b094c79999a0aa227e41527fffabed7db4cf57
mbaridonn <mbaridonn@gmail.com>
2020-03-31 21:09:39 -0300

    Using mocking data for books

--- client/src/Books/Books.tsx
+++ client/src/Books/Books.tsx
@@ -1,5 +1,14 @@
 
 import React from 'react';
+import { BooksMock } from './BooksMock'
 
 export const Books = () =>
-    <h2>Books!</h2>
+    <div>
+        <h2>Books!</h2>
+        {BooksMock.map(book =>
+            <div>
+                <div>{book.name}</div>
+                <div>{book.author}</div>
+            </div>
+        )}
+    </div>
---
+++ client/src/Books/BooksMock.ts
@@ -0,0 +1,14 @@
+export const BooksMock = [
+    {
+        name: "Harry Potter 1",
+        author: "JK Rowling"
+    },
+    {
+        name: "Harry Potter 2",
+        author: "JK Rowling"
+    },
+    {
+        name: "Harry Potter 3",
+        author: "JK Rowling"
+    }
+];
