429f98862f77ac7483e201946c2cfa0815d4e1e1
mbaridonn <mbaridonn@gmail.com>
2020-04-20 21:52:49 -0300

    Basic form to submit a new book

--- client/src/Books/NewBook.tsx
+++ client/src/Books/NewBook.tsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IBook } from './IBook';
+import { Form, Button } from 'react-bootstrap';
 
 export const NewBook = () => {
-    let nameInput: any;
-    let authorInput: any;
 
-    const addBook = async () => {
-        const newBook: IBook = { name: nameInput.value, author: authorInput.value, imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcShCopZfcBk6RUL6wL3JQT8rGCeCc4aD-AY5AqH2XjETlReQ8n0&usqp=CAU' };
-        nameInput.value = '';
-        authorInput.value = '';
+    const addBook = async (form: any) => {
+        var defaultImageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcShCopZfcBk6RUL6wL3JQT8rGCeCc4aD-AY5AqH2XjETlReQ8n0&usqp=CAU';
+        const newBook: IBook = {
+            name: form.formBasicName.value,
+            author: form.formBasicAuthor.value,
+            imageUrl: form.formBasicImageUrl.value
+        };
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -17,18 +19,31 @@ export const NewBook = () => {
         fetch('http://localhost:8080/api/books', requestOptions);
     }
 
+    const handleSubmit = (event: any) => {
+        var form = event.currentTarget.elements;
+        addBook(form);
+    };
+
     return (
-        <div>
-            <h2>New book here!</h2>
-            <input ref={node => {
-                nameInput = node;
-            }} />
-            <input ref={node => {
-                authorInput = node;
-            }} />
-            <button onClick={() => {
-                addBook();
-            }}>Add Book:</button>
-        </div>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="name" placeholder="Enter Name" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicAuthor">
+                <Form.Label>Author</Form.Label>
+                <Form.Control type="author" placeholder="Enter Author" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicImageUrl">
+                <Form.Label>Image Url</Form.Label>
+                <Form.Control type="imageurl" placeholder="Enter Image Url" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 }
