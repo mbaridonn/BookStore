fd5a53c9b617fa2e9b99d51897630e0f064364b2
mbaridonn <mbaridonn@gmail.com>
2021-01-01 19:07:12 -0300

    Basic form validation

--- client/src/Books/NewBook.tsx
+++ client/src/Books/NewBook.tsx
@@ -1,53 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import { IBook } from './IBook';
-import { Form, Button } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { IBook } from "./IBook";
+import { Form, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 export const NewBook = () => {
+  const history = useHistory();
+  const [validated, setValidated] = useState(false);
 
-    const history = useHistory();
-
-    const addBook = async (form: any) => {
-        var defaultImageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcShCopZfcBk6RUL6wL3JQT8rGCeCc4aD-AY5AqH2XjETlReQ8n0&usqp=CAU';
-        const newBook: IBook = {
-            name: form.formBasicName.value,
-            author: form.formBasicAuthor.value,
-            imageUrl: form.formBasicImageUrl.value
-        };
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(newBook)
-        };
-        fetch('http://localhost:8080/api/books', requestOptions);
-    }
-
-    const handleSubmit = (event: any) => {
-        var form = event.currentTarget.elements;
-        addBook(form);
-        history.push('/');
+  const addBook = async (form: any) => {
+    const newBook: IBook = {
+      name: form.formBasicName.value,
+      author: form.formBasicAuthor.value,
+      imageUrl: form.formBasicImageUrl.value,
     };
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newBook),
+    };
+    fetch("http://localhost:8080/api/books", requestOptions);
+  };
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="name" placeholder="Enter Name" />
-            </Form.Group>
+  const handleSubmit = (event: any) => {
+    const validatedForm = event.currentTarget;
+    if (validatedForm.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
 
-            <Form.Group controlId="formBasicAuthor">
-                <Form.Label>Author</Form.Label>
-                <Form.Control type="author" placeholder="Enter Author" />
-            </Form.Group>
+    var form = event.currentTarget.elements;
+    addBook(form);
+    history.push("/");
+  };
 
-            <Form.Group controlId="formBasicImageUrl">
-                <Form.Label>Image Url</Form.Label>
-                <Form.Control type="imageurl" placeholder="Enter Image Url" />
-            </Form.Group>
+  return (
+    <>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control required type="name" placeholder="Enter Name" />
+          <Form.Control.Feedback type="invalid">
+            Please provide a name.
+          </Form.Control.Feedback>
+        </Form.Group>
 
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
-    );
-}
+        <Form.Group controlId="formBasicAuthor">
+          <Form.Label>Author</Form.Label>
+          <Form.Control required type="author" placeholder="Enter Author" />
+          <Form.Control.Feedback type="invalid">
+            Please provide an author.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicImageUrl">
+          <Form.Label>Image Url</Form.Label>
+          <Form.Control
+            required
+            type="imageurl"
+            placeholder="Enter Image Url"
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide an image url.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
