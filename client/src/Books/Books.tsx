7bdbd7f2aefe88d6c82019c9fa395dd49aead5b5
mbaridonn <mbaridonn@gmail.com>
2021-01-02 17:56:01 -0300

    Created Paginator component

--- client/src/Books/Books.tsx
+++ client/src/Books/Books.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
-import { Container, Row, Col, Alert, Pagination } from "react-bootstrap";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 import { IBook } from "./IBook";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { Paginator } from "../Paginator/Paginator";
 
 export const Books = () => {
   const [books, setBooks] = useState<IBook[]>([]);
   const [paginatedBooks, setPaginatedBooks] = useState<IBook[]>([]);
   const [error, setError] = useState(false);
-  const [pages, setPages] = useState<number[]>([]);
-  const [activePage, setActivePage] = useState(1);
   const pageLimit = 3;
 
   const getBooks = async () => {
@@ -20,7 +19,6 @@ export const Books = () => {
       const books: IBook[] = data;
       setBooks(books);
       initFirstPage(books);
-      initTotalPages(books.length);
     } catch (error) {
       setError(true);
     }
@@ -29,24 +27,6 @@ export const Books = () => {
   const initFirstPage = (books: IBook[]) => {
     const firstBooks = books.slice(0, pageLimit);
     setPaginatedBooks(firstBooks);
-  };
-
-  const initTotalPages = (totalBooks: number) => {
-    const totalPages = Math.ceil(totalBooks / pageLimit);
-    const pages = [];
-    for (let i = 1; i <= totalPages; i++) {
-      pages.push(i);
-    }
-    setPages(pages);
-  };
-
-  const changePage = (page: number) => {
-    const start = (page - 1) * pageLimit;
-    const end = page * pageLimit;
-    const selectedBooks = books.slice(start, end);
-
-    setPaginatedBooks(selectedBooks);
-    setActivePage(page);
   };
 
   useEffect(() => {
@@ -73,18 +53,7 @@ export const Books = () => {
           ))}
         </Row>
       </Container>
-      <Container style={{ marginTop: "20px" }}>
-        <Pagination>
-          {pages.map((page) => (
-            <Pagination.Item
-              active={page === activePage}
-              onClick={() => changePage(page)}
-            >
-              {page}
-            </Pagination.Item>
-          ))}
-        </Pagination>
-      </Container>
+      <Paginator books={books} pageLimit={pageLimit} handleClick={setPaginatedBooks}></Paginator>
       {error && (
         <Alert variant="danger">
           There was an error while loading the books.
---
+++ client/src/Paginator/Paginator.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { Container, Pagination } from "react-bootstrap";
+import { IBook } from "../Books/IBook";
+
+interface IPaginatorProps {
+  books: IBook[];
+  pageLimit: number;
+  handleClick: any;
+}
+
+export const Paginator = (props: IPaginatorProps) => {
+  const [pages, setPages] = useState<number[]>([]);
+  const [activePage, setActivePage] = useState(1);
+  const [pageLimit, setPageLimit] = useState(0);
+
+  const changePage = (page: number) => {
+    const start = (page - 1) * pageLimit;
+    const end = page * pageLimit;
+    const selectedBooks = props.books.slice(start, end);
+    props.handleClick(selectedBooks);
+    setActivePage(page);
+  };
+
+  const initTotalPages = (totalBooks: number) => {
+    const totalPages = Math.ceil(totalBooks / pageLimit);
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i);
+    }
+    setPages(pages);
+  };
+
+  useEffect(() => {
+    setPageLimit(props.pageLimit);
+    initTotalPages(props.books.length);
+  }, [props.books]);
+
+  return (
+    <Container style={{ marginTop: "20px" }}>
+      <Pagination>
+        {pages.map((page) => (
+          <Pagination.Item
+            active={page === activePage}
+            onClick={() => changePage(page)}
+          >
+            {page}
+          </Pagination.Item>
+        ))}
+      </Pagination>
+    </Container>
+  );
+};
