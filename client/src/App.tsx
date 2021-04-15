3b01de139472372b198484c91dd7aa8fec910009
mbaridonn <mbaridonn@gmail.com>
2021-04-14 21:08:34 -0300

    Created SiteBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,31 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import { Home } from './Home/Home';
 import { Books } from './Books/Books';
 import { NewBook } from './Books/NewBook';
 import { Book } from './Books/Book';
+import { SiteBar } from './SiteBar/SiteBar';
 
 const App = () =>
   <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/books">Books</Link>
-          </li>
-          <li>
-            <Link to="/books/new">New Book</Link>
-          </li>
-        </ul>
-      </nav>
-
+    <SiteBar/>
       <Switch>
       <Route path="/books/new">
           <NewBook />
@@ -42,8 +28,6 @@ const App = () =>
           <Home />
         </Route>
       </Switch>
-
-    </div>
   </Router>
 
 export default App;
---
+++ client/src/SiteBar/SiteBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+
+export const SiteBar = () => (
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="/">Home</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/books">Books</Nav.Link>
+        <Nav.Link href="/books/new">New Book</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
