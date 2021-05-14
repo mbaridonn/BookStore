02066ece57b365751b1aa0725563dcecb01c6609
Martiniano Baridon <martiniano.baridon@southworks.com>
2021-05-14 15:57:57 -0300

    Books are deleted from the admin panel

--- client/src/AdminPanel/AdminPanel.tsx
+++ client/src/AdminPanel/AdminPanel.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Alert, Button, Table } from "react-bootstrap";
 import { IBook } from "../Books/IBook";
 
 export const AdminPanel = () => {
   const [books, setBooks] = useState<IBook[]>([]);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     const getBooks = async () => {
@@ -22,12 +22,16 @@ export const AdminPanel = () => {
     getBooks();
   }, []);
 
-  const deleteBook = (book: IBook) => {
-    const newBooks: IBook[] = [...books];
-    const realId = getBookId(book.id!);
-    newBooks.splice(realId, 1);
-    setBooks(newBooks);
-    //ToDo: remove book from api
+  const deleteBook = async (book: IBook) => {
+    try {
+      const newBooks: IBook[] = [...books];
+      const realId = getBookId(book.id!);
+      newBooks.splice(realId, 1);
+      await axios.delete(`http://localhost:8080/api/books/${book.id}`);
+      setBooks(newBooks);
+    } catch (error) {
+      alert(`There was an error while deleting ${book.name}`);
+    }
   };
 
   const getBookId = (reqID: string): number => {
@@ -36,27 +40,36 @@ export const AdminPanel = () => {
   };
 
   return (
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Author</th>
-          <th>Image</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map((book) => (
+    <div>
+      <Table striped bordered hover variant="dark">
+        <thead>
           <tr>
-            <td>{book.name}</td>
-            <td>{book.author}</td>
-            <td>{book.imageUrl}</td>
-            <td>
-              <Button onClick={() => deleteBook(book)}>Delete</Button>
-            </td>
+            <th>Name</th>
+            <th>Author</th>
+            <th>Image</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr>
+              <td>{book.name}</td>
+              <td>{book.author}</td>
+              <td>{book.imageUrl}</td>
+              <td>
+                <Button onClick={async () => await deleteBook(book)}>
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      {error && (
+        <Alert variant="danger">
+          There was an error while loading the books.
+        </Alert>
+      )}
+    </div>
   );
 };
